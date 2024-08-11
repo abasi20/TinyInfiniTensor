806528864e3f16330ceeb5eb9d63bad59f9349ef
abasi20 <jungege321@126.com>
2024-08-11 20:32:37 +0800

    transpose

--- src/operators/transpose.cc
+++ src/operators/transpose.cc
@@ -37,7 +37,7 @@ namespace infini
         {
             output_dim[i] = input_dim[transposePermute[i]];
         }
-        return vector<Shape>(inputs.size(), output_dim);
+        return {{output_dim}};
     }
 
     std::string TransposeObj::toString() const
